59e01c927107bca53b61dd217d157d4123c4a5fa
Prabhjot Singh <pjsingh1359@gmail.com>
2023-04-14 12:59:05 -0700

    comit

--- backend/db/migrations/20230413202432-create-user.js
+++ backend/db/migrations/20230413202432-create-user.js
@@ -43,6 +43,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Users';
-    await queryInterface.dropTable(options);
+    await queryInterface.dropTable('Users', options);
   }
 };
