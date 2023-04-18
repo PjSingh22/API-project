1461c7250c87db02bce0c05817c1b8503193a39d
Prabhjot Singh <pjsingh1359@gmail.com>
2023-04-17 20:45:26 -0700

    add routes to get and edit post by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,6 +2,48 @@ const express = require('express');
 const router = express.Router();
 
 const { Spot, SpotImage } = require('../../db/models');
+const spot = require('../../db/models/spot');
+
+// TODO: add reviews and avgRating to returned data
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const spot = await Spot.findByPk(id);
+
+  if (spot) {
+    return res.json(spot);
+  }
+
+  return res.status(404).json(`spot with id ${id} does not exist.`);
+})
+
+// TODO: add reviews to this.
+router.put('/:id', async (req, res) => {
+  const { user } = req;
+  const spotToEdit = await Spot.findByPk(id, {
+    where: {
+      ownerId: user.id
+    }
+  });
+
+  if (spotToEdit) {
+    const { address, city, state, country, lat, lng, name, description, price} = req.body;
+
+    if (address) spotToEdit.address = address;
+    if (city) spotToEdit.city = city;
+    if (state) spotToEdit.state = state;
+    if (country) spotToEdit.country = country;
+    if (lat) spotToEdit.lat = lat;
+    if (lng) spotToEdit.lng = lng;
+    if (name) spotToEdit.name = name;
+    if (description) spotToEdit.name = name;
+    if (price) spotToEdit.price = price;
+
+    await spotToEdit.save();
+    return res.json(spotToEdit);
+  }
+
+  return res.status(404).json('Spot does not exist');
+})
 
 router.post('/:id/images', async (req, res) => {
   const { user } = req;
