3362fa5cf62c23a35c284672e27b595d0bb7bd7e
Prabhjot Singh <pjsingh1359@gmail.com>
2023-05-05 10:33:22 -0700

    phase 2 done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -37,7 +37,24 @@ export const login = (user) => async (dispatch) => {
     dispatch(fetchUser(data.user));
     return response;
   }
-}
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(fetchUser(data.user));
+  return response;
+};
 
 // export const logoutUserThunk = () => async (dispatch) => {
 
