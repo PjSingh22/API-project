e1548d9ff44346c85c413aff29b5b4da29dbeeb3
Prabhjot Singh <pjsingh1359@gmail.com>
2023-05-08 16:05:17 -0700

    add reviews redux store

--- frontend/src/components/ViewSpot/index.js
+++ frontend/src/components/ViewSpot/index.js
@@ -2,15 +2,18 @@ import { useEffect, useReducer, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { getSpotThunk } from "../../store/spots";
+import { getSpotReviewsThunk } from "../../store/reviews";
 import './ViewSpot.css';
 
 const ViewSpot = ({defaultImg}) => {
   const { spotId } = useParams();
   const dispatch = useDispatch();
   const spotObj = useSelector(state => state.spots.singleSpot);
+  const reviewsObj = useSelector(state => state.reviews)
   const { address, avgStarRating, city, country, description, lat, lng, name, numReviews, owner, price, spotImages, state } = spotObj;
 
   useEffect(() => {
+    dispatch(getSpotReviewsThunk(spotId));
     dispatch(getSpotThunk(spotId))
   }, [dispatch]);
 
@@ -28,15 +31,15 @@ const ViewSpot = ({defaultImg}) => {
             <img src={spotImages[0].url} alt="home" />
           </div>
           <div className="other-imgs">
-            <img src={spotImages[1] ? spotImages[1].url : defaultImg} alt="home" />
             <img src={spotImages[2] ? spotImages[2].url : defaultImg} alt="home" />
             <img src={spotImages[3] ? spotImages[3].url : defaultImg} alt="home" />
             <img src={spotImages[4] ? spotImages[4].url : defaultImg} alt="home" />
+            <img src={spotImages[5] ? spotImages[5].url : defaultImg} alt="home" />
           </div>
         </div>
         <div className="spot-info">
           <div className="spot-info-left">
-            <p style={{fontSize: "20px"}}>Hosted by {owner.firstName} {owner.lastName}</p>
+            <p style={{fontSize: "1.3eem"}}>Hosted by {owner.firstName} {owner.lastName}</p>
             <p className="spot__desc">{description}</p>
           </div>
           <div className="spot-info-right">
@@ -47,7 +50,9 @@ const ViewSpot = ({defaultImg}) => {
             <button className="btn login-btn" onClick={() => alert('feature coming soon')}>Reserve</button>
           </div>
         </div>
-        <div className="spot-reviews"></div>
+        <div className="spot-reviews">
+        <p style={{fontSize: "1.2em"}}><i className="fa-solid fa-star"></i> {avgStarRating} * {numReviews} Reviews</p>
+        </div>
       </div>
     )
 };
---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,35 @@
+import { csrfFetch } from "./csrf";
+
+const GET_REVIEWS = "spots/GET_REVIEWS";
+
+const getSpotReviews = (reviews) => {
+  return {
+    type: GET_REVIEWS,
+    reviews
+  }
+};
+
+export const getSpotReviewsThunk = (spotId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/spots/${spotId}/reviews`);
+  const reviews = await response.json();
+  console.log('reviews', reviews);
+  dispatch(getSpotReviews(reviews.Reviews));
+}
+
+const initialState = { spot: {}, user: {} };
+
+const reviewsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_REVIEWS:
+      const reviewsState = {};
+      action.reviews.forEach(review => {
+        reviewsState[review.id] = review;
+      });
+      return { ...state, spot: reviewsState}
+      // return state;
+    default:
+      return state;
+  }
+}
+
+export default reviewsReducer;
